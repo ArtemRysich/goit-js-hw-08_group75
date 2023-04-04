bd2fd0d2ceab529c21f66c7eaf83b470d028a1fa
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2023-04-04 17:50:37 +0000

    Deploying to gh-pages from @ ArtemRysich/goit-js-hw-08_group75@8c27e7a077670a846df2b86ed41ea804baaee9d7 🚀

---
+++ 01-gallery.0796d461.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),value=10,console.log(10);
+//# sourceMappingURL=01-gallery.0796d461.js.map
---
+++ 01-gallery.436c499b.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),value=10,console.log(value);
+//# sourceMappingURL=01-gallery.436c499b.js.map
